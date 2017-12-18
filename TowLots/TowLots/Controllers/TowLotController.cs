b9529e03403ce90b404fc72f9c35e2e5e0511873
Abdikader <gahnuge@aol.com>
2017-12-17 23:57:08 -0600

    Added Model Class, Controller

---
+++ TowLots/TowLots/Controllers/TowLotController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TowLots.Models;
+
+namespace TowLots.Controllers
+{
+    public class TowLotController: Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri("https://data.kcmo.org")
+            };
+            var response = await client.GetStringAsync($"/resource/xpwx-fzzw.json?$$app_token=RywPlFpEm1SQhxfw6lTHBbcC9");
+            response.En
+            var result = await response.Con
+            var data = JsonConvert.DeserializeObject<IEnumerable<TowLot>>(response);
+            
+
+            return View(data);
+        }
+    }
+}
---
+++ TowLots/TowLots/Models/TowLot.cs
@@ -0,0 +1,17 @@
+﻿namespace TowLots.Models
+{
+    public class TowLot
+    {
+        public int Lot { get; set; }
+        public int VehicleId { get; set; }
+        public int TowReference { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Vin { get; set; }
+        public string Mileage { get; set; }
+        public string Keys { get; set; }
+        public string Reason { get; set; }
+        public string Comments { get; set; }
+    }
+}

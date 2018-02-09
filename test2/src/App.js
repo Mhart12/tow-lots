f17c3b8c530500f16663c0ccf667ee0132f9c647
Steve Bosilovatz <bosilovatz@gmail.com>
2018-02-08 18:15:25 -0600

    Update folder structure, correct drop down render

--- test2/src/App.js
+++ test2/src/App.js
@@ -1,175 +1,22 @@
 import React from 'react';
-import axios from 'axios';
-import $ from 'jquery'; 
 
 import classes from './App.css'
-import VehicleCell from './components/VehicleCell/VehicleCell'
-import SearchFunctions from './components/SearchFunctions/SearchFunctions'
-import SearchButton from './components/SearchFunctions/SearchButton/SearchButton'
+import SearchItems from './components/SearchItems/SearchItems'
 import Toolbar from './components/Toolbar/Toolbar'
-
-// global variables assigned based on drop down selections
-let dropDownYear = ""
-let dropDownMake = ""
-let dropDownReason = ""
-let dropDownKey = ""
-let dropDownModel = ""
-
-// when drop menu item is clicked, assign variable to be used later
-$(function() {
-  $("#makeDropDown").on("change",function() {
-    dropDownMake = this.value;
-  });
-});
-
-$(function() {
-  $("#keyDropDown").on("change",function() {
-    dropDownKey = this.value;
-  });
-});
-
-$(function() {
-  $("#yearDropDown").on("change",function() {
-    dropDownYear = this.value;
-  });
-});
-
-$(function() {
-  $("#reasonDropDown").on("change",function() {
-    dropDownReason = this.value;
-  });
-});
-
-$(function() {
-  $("#modelDropDown").on("change",function() {
-    dropDownModel = this.value;
-  });
-});
+import ShowResults from './components/ShowResults/ShowResults'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      data: [],
-      clicked: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  // when search button is clicked, it will rerender the render component below
-  handleClick() {
-    this.setState({
-      clicked: true,
-    });
-  }
-  
-  // get request to get JSON data
-  componentDidMount(){
-    axios
-      .get('https://data.kcmo.org/resource/xpwx-fzzw.json')
-      .then(({data})=> {
-        this.setState({ 
-          data: data,
-        });
-      }).catch((err)=> {})
-  }
    
   render() {
 
-    // iterate through JSON data and set variables 
-    const child = this.state.data.map((el, index) => {
-
-      let keys
-      if (el.k === undefined) keys = "NO"
-      if (el.k === " ") keys = "NO"
-      if (el.k === "K") keys = "YES"
-
-      let comments = el.comments;
-      if (comments === undefined) comments = ""
-  
-      let year = el.year,
-      make = el.make,
-      model = el.model,
-      reason = el.reason,
-      lot = el.lot,
-      towReference = el.tow_reference,
-      vehicleID = el.vehicle_id,
-      vin = el.vin,
-
-      displayVehicleCell =
-      <VehicleCell 
-                key= {vehicleID} 
-                year = {year} 
-                make = {make} 
-                vin = {vin}
-                model = {model} 
-                reason = {reason}
-                lot = {lot} 
-                tow_reference = {towReference}
-                vehicle_id = {vehicleID}
-                k = {keys}
-                comment = {comments} />
-
-      // search by only year
-      if (year === dropDownYear && dropDownMake === "" && dropDownKey === "" && dropDownReason === "") {
-        return ( <div> {displayVehicleCell} </div> )
-      }
-      // search by only make
-      else if (make === dropDownMake && dropDownYear === "" && dropDownModel === "" && dropDownKey === "" && dropDownReason === "") {
-        return ( <div> {displayVehicleCell} </div>)  
-      }
-      // search only by keys
-      else if (dropDownMake === "" && dropDownYear === "" && dropDownModel === "" && keys === dropDownKey && dropDownReason === "") {
-        return ( <div> {displayVehicleCell} </div> ) 
-      }
-      // search only by reason
-      else if (dropDownMake === "" && dropDownYear === "" && dropDownModel === "" && dropDownKey === "" && reason === dropDownReason) {
-        return ( <div> {displayVehicleCell} </div> ) 
-      }
-      // search by only make and model
-      else if (make === dropDownMake && dropDownYear === "" && model === dropDownModel && dropDownKey === "" && dropDownReason === "") {
-        return ( <div> {displayVehicleCell} </div> ) 
-      }
-      // search by only make and year
-      else if (make === dropDownMake && year === dropDownYear && dropDownModel === "" && dropDownKey === "" && dropDownReason === "") {
-        return ( <div> {displayVehicleCell} </div> ) 
-      }
-      // search by only year and keys
-      else if (dropDownMake === "" && year === dropDownYear && dropDownModel === "" && keys === dropDownKey && dropDownReason === "") {
-        return ( <div> {displayVehicleCell} </div>) 
-      }
-      // search by only year and reason
-      else if (dropDownMake === "" && year === dropDownYear && dropDownModel === "" && dropDownKey === "" && reason === dropDownReason) {
-        return ( <div> {displayVehicleCell} </div> ) 
-      }
-      // search by make and reason
-      else if (make === dropDownMake && dropDownYear === "" && dropDownModel === "" && dropDownKey === "" && reason === dropDownReason) {
-        return ( <div> {displayVehicleCell} </div> ) 
-      }
-      // search by make and keys
-      else if (make === dropDownMake && dropDownYear === "" && dropDownModel === "" && keys === dropDownKey && dropDownReason === "") {
-        return ( <div> {displayVehicleCell} </div> ) 
-      }
-      // search by keys and reason
-      else if (dropDownMake === "" && dropDownYear === "" && dropDownModel === "" && keys === dropDownKey && reason === dropDownReason) {
-        return ( <div> {displayVehicleCell} </div> ) 
-      }
-      // search by year make model
-      else if (make === dropDownMake && year === dropDownYear && model === dropDownModel && dropDownKey === "" && dropDownReason === "") {
-        return ( <div> {displayVehicleCell} </div> ) 
-      } 
-    })
-  
-    // data that gets passed to index.js and is displayed
     return <div className ={classes.containerStyle}>
       <Toolbar />   
       <div className ={classes.searchDivStyle}>
-        <SearchFunctions />
-        <SearchButton onClick={this.handleClick} />
+        <SearchItems/>
       </div>
-      <div>{child}</div>
+      <ShowResults />
     </div>;
+
   }
 }
 
---
+++ test2/src/components/SearchItems/SearchItems.js
@@ -0,0 +1,174 @@
+import React from 'react';
+import axios from 'axios';
+import $ from 'jquery'; 
+
+import classes from './SearchItems.css'
+
+class searchFunctions extends React.Component {
+	constructor() {
+	    super();	
+
+	  	this.state = {
+	      data: [],
+	      makes: [],
+	      years: []
+	    };
+	}
+
+	componentWillMount(){
+	    axios
+	      .get('https://data.kcmo.org/resource/xpwx-fzzw.json')
+	      .then(({data})=> {
+	        this.setState({ 
+	          data: data,
+	        });
+
+	        // create array of available years, remove duplicates, sort from big to small
+		    let years = this.state.data.map((el, index) => { return el.year })
+		    let filteredYearsArray = years.filter(function(el, index) {
+				return years.indexOf(el) === index;
+			})
+		    filteredYearsArray.sort((a,b) => {return b-a })
+
+		    // create array of available makes, remove duplicates, sort alphabetically
+			let makes = this.state.data.map((el, index) => { return el.make })
+		    let filteredMakesArray = makes.filter(function(el, index) {
+				return makes.indexOf(el) === index;
+			})
+			filteredMakesArray.sort(function (a, b) {
+		      if (b > a) return -1;
+		      else if (b < a) return 1;
+		      return 0;
+		    });
+
+		    this.setState({
+            	makes: filteredMakesArray,
+            	years: filteredYearsArray,
+        	})
+
+	    }).catch((err)=> {})
+	}
+
+	render() {
+
+		let json = this.state.data
+		let makesArray = this.state.makes
+		let yearsArray = this.state.years
+
+		// append years to drop list 
+		$('#yearDropDown').empty();
+		$('#yearDropDown').append($('<option></option>').val("").html(""));
+		$.each(yearsArray, function(i, p) {
+		    $('#yearDropDown').append($('<option></option>').val(p).html(p));
+		});
+
+		// append makes to drop list
+		$('#makeDropDown').empty();
+		$('#makeDropDown').append($('<option></option>').val("").html(""));
+		$.each(makesArray, function(i, p) {
+		    $('#makeDropDown').append($('<option></option>').val(p).html(p));
+		});
+
+		// when make is selected, show available models in model dropdown
+		$(function() {
+    		$("#makeDropDown").on("change",function() {
+      			let selectedMake = this.value;
+
+      			// find objects that are equal to selected make ie BMW, FORD
+				let filterByMake = json.filter((el, index) => {
+					return el.make === selectedMake;
+				})
+
+				// return only the model names
+				let modelsArray = filterByMake.map((el, index) => { 
+					return el.model
+				})
+
+				// remove any duplicate model names
+				modelsArray = modelsArray.filter(function(el, index) {
+					return modelsArray.indexOf(el) === index;
+				})
+
+				// append to drop down list
+				$('#modelDropDown').empty();
+				$('#modelDropDown').append($('<option></option>').val("").html(""));
+				$.each(modelsArray, function(i, p) {
+				    $('#modelDropDown').append($('<option></option>').val(p).html(p));
+				});			
+    		});
+  		});
+
+		// when year is selected, show available makes in make dropdown 
+		$(function() {
+    		$("#yearDropDown").on("change",function() {
+      			let selectedYear = this.value;
+
+      			// find objects that are equal to selected year
+				let filterByYear = json.filter((el, index) => {
+					return el.year === selectedYear;
+				})
+
+				// return only the model names
+				let yearsArr = filterByYear.map((el, index) => { 
+					return el.make
+				})
+
+				// remove any duplicate model names
+				yearsArr = yearsArr.filter(function(el, index) {
+					return yearsArr.indexOf(el) === index;
+				})
+
+				// append to drop down list
+				$('#makeDropDown').empty();
+				$('#modelDropDown').empty();
+				$('#makeDropDown').append($('<option></option>').val("").html(""));
+				$.each(yearsArr, function(i, p) {
+				    $('#makeDropDown').append($('<option></option>').val(p).html(p));
+				});			
+    		});
+  		});
+
+
+		return <div className = {classes.SearchItems}> 
+			<table>
+				<tr>
+					<td> Year </td>
+					<td>
+						<select id = "yearDropDown"> </select>
+					</td>	
+					<td> Make </td>
+					<td>
+						<select id = "makeDropDown" style = {{width: '150px'}}> </select>
+					</td>
+
+					<td> Model </td>
+					<td>
+						<select id = "modelDropDown" style = {{width: '150px'}}> </select>
+					</td>	
+					<td> Keys </td>
+					<td>
+						<select id = "keyDropDown">
+							<option value= ""></option>
+							<option value= "YES"> YES </option>
+							<option value= "NO"> NO </option>
+						</select>
+					</td>
+					<td> Reason </td>
+					<td>
+						<select id = 'reasonDropDown'>
+							<option value= ''> </option>
+							<option value= 'ABANDONED'> ABANDONED </option>
+							<option value= 'ACCIDENT'> ACCIDENT </option>
+							<option value= 'ARREST'> ARREST </option>
+							<option value= 'ILLEGALLY PARKED'> ILLEGALLY PARKED</option>
+							<option value= 'OTHER'> OTHER </option>
+							<option value= 'STOLEN'> STOLEN </option>	
+						</select>
+					</td>
+				</tr>				
+			</table>
+		</div>
+	}
+}
+
+export default searchFunctions;

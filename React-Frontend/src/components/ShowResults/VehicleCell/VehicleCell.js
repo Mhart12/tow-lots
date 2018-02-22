2ccdf7dcda744e674ed28d53974468c90ae2fcb2
bosilovatz <bosilovatz@gmail.com>
2018-02-22 15:24:40 -0600

    Add Vin decoding and update some css

--- React-Frontend/src/components/ShowResults/VehicleCell/VehicleCell.js
+++ React-Frontend/src/components/ShowResults/VehicleCell/VehicleCell.js
@@ -1,15 +1,37 @@
 import React from 'react';
+
+import $ from 'jquery'; 
 import firebase from 'firebase';
 
 import classes from './VehicleCell.css';
+import DecodeVIN from '../DecodeVIN/DecodeVIN'
 
 class vehicleCell extends React.Component {
     constructor(props) {
         super(props);
         this.saveItem = this.saveItem.bind(this);
+        this.getVinInformation = this.getVinInformation.bind(this);
         this.state = {
             vehicle: '',
-            user: ''
+            user: '',
+            infoButton: 'More',
+            showPopup: false,
+            transmission: '',
+            driveType: '',
+            ABS: '',
+            bodyClass: '',
+            engineHP: '',
+            vehicleType: '',
+            doors: '',
+            displacement: '',
+            engineCylinders: '',
+            engineConfig: '',
+            fuelType: '',
+            frontAirBag: '',
+            length: '',
+            weight: '',
+            seatBelts: '',
+
         };
     }
 
@@ -23,8 +45,62 @@ class vehicleCell extends React.Component {
             vehicle: '',
             user: ''
         });
-        alert(`Saving...`)
     }   
+
+    getVinInformation () {
+
+        let transmission, driveType, ABS, bodyClass, engineHP, vehicleType, doors, displacementCC,
+            engineCylinders, engineConfig, fuelType, frontAirBag, length, weight, seatBelts
+
+        $.ajax({ 
+            url: "https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVINValuesBatch/",
+            async: false,
+            type: "POST",
+            data: { format: "json", data: this.props.vin},
+            dataType: "json",
+            success: function(result) {
+                console.log(result.Results[0]);
+
+                transmission = result.Results[0].TransmissionStyle
+                driveType = result.Results[0].DriveType
+                ABS = result.Results[0].ABS
+                bodyClass = result.Results[0].BodyClass
+                engineHP = result.Results[0].EngineHP
+                vehicleType = result.Results[0].VehicleType 
+                doors = result.Results[0].Doors
+                displacementCC = result.Results[0].DisplacementCC
+                engineCylinders = result.Results[0].EngineCylinders
+                engineConfig = result.Results[0].EngineConfiguration
+                fuelType = result.Results[0].FuelTypePrimary
+                frontAirBag = result.Results[0].AirBagLocFront
+                length = result.Results[0].TrailerLength
+                weight = result.Results[0].GVWR
+                seatBelts = result.Results[0].SeatBeltsAll
+
+            }
+        })
+
+        this.setState({
+            showPopup: !this.state.showPopup,
+            infoButton: this.state.showPopup ? 'More' : 'Less',
+            transmission: transmission,
+            driveType: driveType,
+            ABS: ABS,
+            bodyClass: bodyClass,
+            engineHP: engineHP,
+            vehicleType: vehicleType,
+            doors: doors,
+            displacementCC: displacementCC,
+            engineCylinders: engineCylinders,
+            engineConfig: engineConfig,
+            fuelType: fuelType,
+            frontAirBag: frontAirBag,
+            length: length,
+            weight: weight,
+            seatBelts: seatBelts
+
+        })
+    }
 
     render() {
 
@@ -49,10 +125,34 @@ class vehicleCell extends React.Component {
                                 </td>
                                 <td>
                                     <button className = {classes.saveButton} onClick = {this.saveItem}> Save Vehicle </button>
+                                    <button className = {classes.saveButton} 
+                                            onClick = {this.getVinInformation.bind(this)}> {this.state.infoButton} Vehicle Information </button>
                                 </td>
                             </tr>
                         </tbody>
                     </table>
+                </div>
+                <div className = {classes.vinCell}>
+                    {this.state.showPopup ? 
+                            <DecodeVIN 
+                                transmission = {this.state.transmission}
+                                driveType = {this.state.driveType}
+                                ABS = {this.state.ABS}
+                                bodyClass = {this.state.bodyClass}
+                                engineHP = {this.state.engineHP}
+                                vehicleType = {this.state.vehicleType}
+                                doors = {this.state.doors}
+                                displacementCC = {this.state.displacementCC}
+                                engineCylinders = {this.state.engineCylinders}
+                                engineConfig = {this.state.engineConfig}
+                                fuelType = {this.state.fuelType}
+                                frontAirBag = {this.state.frontAirBag}
+                                length = {this.state.length}
+                                weight = {this.state.weight}
+                                seatBelts = {this.state.seatBelts}
+                                closePopup = {this.getVinInformation.bind(this)}
+                            /> : null
+                        }
                 </div>
             </div>
         )

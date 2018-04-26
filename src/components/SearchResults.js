3f8c367b4f12390715631f2e02895e34ea630eeb
Steve Bosilovatz <bosilovatz@gmail.com>
2018-04-26 17:56:53 -0500

    Refactor code

---
+++ src/components/SearchResults.js
@@ -0,0 +1,162 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Row, Spin } from 'antd';
+import SearchItem from './SearchItem'
+
+export default class SearchResults extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://data.kcmo.org/resource/xpwx-fzzw.json')
+      .then(({data})=> {
+        this.setState({
+          data: data,
+          loading: false
+        });
+    }).catch((err)=> {})
+  }
+
+  returnResults(year, make, model, key, reason) {
+    // onload dsiplay all vehicles since this items are not yet defined
+    if (year === undefined && make === undefined && model === undefined && key === undefined && reason === undefined) {
+      return (
+        this.state.loading === true ? <Spin /> : this.state.data.sort((a,b) => a.lot - b.lot).map((e, key) =>
+          <div key = {key}>
+            <SearchItem
+              year = {e.year}
+              make = {e.make}
+              model = {e.model}
+              vin = {e.vin}
+              lot = {e.lot}
+              reason = {e.reason}
+              tow_reference = {e.tow_reference}
+              vehicle_id = {e.vehicle_id}
+              comments = {e.comments}
+              keys = {e.k} />
+          </div> ))
+    // search by only year
+  } else if (year !== undefined && make === undefined && model === undefined && key === undefined && reason === undefined) {
+      return (
+        this.state.loading === true ? <Spin /> : this.state.data.filter(x => x.year === this.props.searchYear).sort((a,b) => a.lot - b.lot).map((e, key) =>
+          <div key = {key}>
+            <SearchItem
+              year = {e.year}
+              make = {e.make}
+              model = {e.model}
+              vin = {e.vin}
+              lot = {e.lot}
+              reason = {e.reason}
+              tow_reference = {e.tow_reference}
+              vehicle_id = {e.vehicle_id}
+              comments = {e.comments}
+              keys = {e.k} />
+          </div> ))
+      // search by only make
+    } else if (year === undefined && make !== undefined && model === undefined && key === undefined && reason === undefined) {
+        return (
+          this.state.loading === true ? <Spin /> : this.state.data.filter(x => x.make === this.props.searchMake).sort((a,b) => a.lot - b.lot).map((e, key) =>
+          <div key = {key}>
+            <SearchItem
+              year = {e.year}
+              make = {e.make}
+              model = {e.model}
+              vin = {e.vin}
+              lot = {e.lot}
+              reason = {e.reason}
+              tow_reference = {e.tow_reference}
+              vehicle_id = {e.vehicle_id}
+              comments = {e.comments}
+              keys = {e.k} />
+          </div> ))
+        // search by only model
+      } else if (year === undefined && make === undefined && model !== undefined && key === undefined && reason === undefined) {
+          return (
+            this.state.loading === true ? <Spin /> : this.state.data.filter(x => x.model === this.props.searchModel).sort((a,b) => a.lot - b.lot).map((e, key) =>
+            <div key = {key}>
+              <SearchItem
+                year = {e.year}
+                make = {e.make}
+                model = {e.model}
+                vin = {e.vin}
+                lot = {e.lot}
+                reason = {e.reason}
+                tow_reference = {e.tow_reference}
+                vehicle_id = {e.vehicle_id}
+                comments = {e.comments}
+                keys = {e.k} />
+            </div> ))
+        // search by only keys
+        } else if (year === undefined && make === undefined && model === undefined && key !== undefined && reason === undefined) {
+            return (
+              this.state.loading === true ? <Spin /> : this.state.data.filter(x => (x.k === 'K' ? 'Yes' : 'No') === this.props.searchKey).sort((a,b) => a.lot - b.lot).map((e, key) =>
+              <div key = {key}>
+                <SearchItem
+                  year = {e.year}
+                  make = {e.make}
+                  model = {e.model}
+                  vin = {e.vin}
+                  lot = {e.lot}
+                  reason = {e.reason}
+                  tow_reference = {e.tow_reference}
+                  vehicle_id = {e.vehicle_id}
+                  comments = {e.comments}
+                  keys = {e.k} />
+              </div> ))
+          // search by only reason
+          } else if (year === undefined && make === undefined && model === undefined && key === undefined && reason !== undefined) {
+                return (
+                  this.state.loading === true ? <Spin /> : this.state.data.filter(x => x.reason === this.props.searchReason).sort((a,b) => a.lot - b.lot).map((e, key) =>
+                  <div key = {key}>
+                    <SearchItem
+                      year = {e.year}
+                      make = {e.make}
+                      model = {e.model}
+                      vin = {e.vin}
+                      lot = {e.lot}
+                      reason = {e.reason}
+                      tow_reference = {e.tow_reference}
+                      vehicle_id = {e.vehicle_id}
+                      comments = {e.comments}
+                      keys = {e.k} />
+                  </div> ))
+          // search by year and make
+        } else if (year !== undefined && make !== undefined && model === undefined && key === undefined && reason === undefined) {
+            return (
+              this.state.loading === true ? <Spin /> : this.state.data
+                .filter(x => x.year === this.props.searchYear && x.make === this.props.searchMake)
+                .sort((a,b) => a.lot - b.lot)
+                .map((e, key) =>
+                  <div key = {key}>
+                    <SearchItem
+                      year = {e.year}
+                      make = {e.make}
+                      model = {e.model}
+                      vin = {e.vin}
+                      lot = {e.lot}
+                      reason = {e.reason}
+                      tow_reference = {e.tow_reference}
+                      vehicle_id = {e.vehicle_id}
+                      comments = {e.comments}
+                      keys = {e.k} />
+                  </div> ))
+        }
+  }
+
+    render() {
+      return (
+        <div>
+          <Row gutter={32} style = {{textAlign: 'center', marginTop: 20}}>
+            {this.returnResults(this.props.searchYear, this.props.searchMake, this.props.searchModel, this.props.searchKey, this.props.searchReason)}
+          </Row>
+        </div>
+      )
+    }
+}

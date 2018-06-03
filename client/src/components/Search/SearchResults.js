c24e60c73196a5815de6e2e196d40f421ff48d88
Steve Bosilovatz <bosilovatz@gmail.com>
2018-06-03 14:17:13 -0500

    Starting to add functional pagination

--- client/src/components/Search/SearchResults.js
+++ client/src/components/Search/SearchResults.js
@@ -9,8 +9,11 @@ export default class SearchResults extends Component {
 
     this.state = {
       data: [],
-      loading: true
+      loading: true,
+      pageStart: 0,
+      pageEnd: 9,
     }
+    this.onChange = this.onChange.bind(this);
   }
 
   componentDidMount() {
@@ -19,9 +22,20 @@ export default class SearchResults extends Component {
       .then(({data})=> {
         this.setState({
           data: data.sort((a,b) => a.lot - b.lot),
-          loading: false
+          loading: false,
         });
     }).catch((err)=> {})
+  }
+
+  onChange(page, pageSize) {
+    page = page - 1
+
+    this.setState({
+      pageStart: page * pageSize,
+      pageEnd: page * pageSize + pageSize
+    })
+
+    window.scrollTo(0, 0)
   }
 
   returnResults(year, make, model, key, reason) {
@@ -32,16 +46,17 @@ export default class SearchResults extends Component {
           <div key = {key}>
             <SearchItem year = {e.year} make = {e.make} model = {e.model} vin = {e.vin} lot = {e.lot} reason = {e.reason} tow_reference = {e.tow_reference} vehicle_id = {e.vehicle_id} comments = {e.comments} keys = {e.k} front_pic = {e.front_pic} back_pic = {e.back_pic} />
           </div>
-        )
+        ).slice(this.state.pageStart, this.state.pageEnd)
       )
     // search by only year
     } else if (year !== "" && make === "" && model === "" && key === "" && reason === "") {
+
       return (
         this.state.loading === true ? <Spin /> : this.state.data.filter(x => x.year === this.props.searchYear).map((e, key) =>
           <div key = {key}>
             <SearchItem year = {e.year} make = {e.make} model = {e.model} vin = {e.vin} lot = {e.lot} reason = {e.reason} tow_reference = {e.tow_reference} vehicle_id = {e.vehicle_id} comments = {e.comments} keys = {e.k} front_pic = {e.front_pic} back_pic = {e.back_pic} />
           </div>
-        )
+        ).slice(this.state.pageStart, this.state.pageEnd)
       )
       // search by only make
     } else if (year === "" && make !== "" && model === "" && key === "" && reason === "") {
@@ -50,81 +65,88 @@ export default class SearchResults extends Component {
             <div key = {key}>
               <SearchItem year = {e.year} make = {e.make} model = {e.model} vin = {e.vin} lot = {e.lot} reason = {e.reason} tow_reference = {e.tow_reference} vehicle_id = {e.vehicle_id} comments = {e.comments} keys = {e.k} front_pic = {e.front_pic} back_pic = {e.back_pic} />
             </div>
-          )
+          ).slice(this.state.pageStart, this.state.pageEnd)
         )
         // search by only model
-      } else if (year === "" && make === "" && model !== "" && key === "" && reason === "") {
-          return (
-            this.state.loading === true ? <Spin /> : this.state.data.filter(x => x.model === this.props.searchModel).map((e, key) =>
-              <div key = {key}>
-                <SearchItem year = {e.year} make = {e.make} model = {e.model} vin = {e.vin} lot = {e.lot} reason = {e.reason} tow_reference = {e.tow_reference} vehicle_id = {e.vehicle_id} comments = {e.comments} keys = {e.k} front_pic = {e.front_pic} back_pic = {e.back_pic} />
-              </div>
-            )
-          )
-        // search by only keys
-        } else if (year === "" && make === "" && model === "" && key !== "" && reason === "") {
-            return (
-              this.state.loading === true ? <Spin /> : this.state.data.filter(x => (x.k === 'K' ? 'Yes' : 'No') === this.props.searchKey).map((e, key) =>
-              <div key = {key}>
-                <SearchItem year = {e.year} make = {e.make} model = {e.model} vin = {e.vin} lot = {e.lot} reason = {e.reason} tow_reference = {e.tow_reference} vehicle_id = {e.vehicle_id} comments = {e.comments} keys = {e.k} front_pic = {e.front_pic} back_pic = {e.back_pic} />
-              </div>
-              )
-            )
-          // search by only reason
-          } else if (year === "" && make === "" && model === "" && key === "" && reason !== "") {
-              return (
-                this.state.loading === true ? <Spin /> : this.state.data.filter(x => x.reason === this.props.searchReason).map((e, key) =>
-                  <div key = {key}>
-                    <SearchItem year = {e.year} make = {e.make} model = {e.model} vin = {e.vin} lot = {e.lot} reason = {e.reason} tow_reference = {e.tow_reference} vehicle_id = {e.vehicle_id} comments = {e.comments} keys = {e.k} front_pic = {e.front_pic} back_pic = {e.back_pic} />
-                  </div>
-                )
-              )
-          // search by year and make
-          } else if (year !== "" && make !== "" && model === "" && key === "" && reason === "") {
-            let results =
-            this.state.loading === true ? <Spin /> :
-            this.state.data
-              .filter(x => x.year === this.props.searchYear && x.make === this.props.searchMake)
-              .map((e, key) =>
-                <div key = {key}>
-                  <SearchItem year = {e.year} make = {e.make} model = {e.model} vin = {e.vin} lot = {e.lot} reason = {e.reason} tow_reference = {e.tow_reference} vehicle_id = {e.vehicle_id} comments = {e.comments} keys = {e.k} front_pic = {e.front_pic} back_pic = {e.back_pic} />
-                </div>
-              )
-            if (results.length === 0) message.error('No Results Found');
-            return (results)
-           // search by year and model
-         } else if (year !== "" && make === "" && model !== "" && key === "" && reason === "") {
-            let results =
-            this.state.loading === true ? <Spin /> :
-            this.state.data
-              .filter(x => x.year === this.props.searchYear && x.model === this.props.searchModel)
-              .map((e, key) =>
-                <div key = {key}>
-                  <SearchItem year = {e.year} make = {e.make} model = {e.model} vin = {e.vin} lot = {e.lot} reason = {e.reason} tow_reference = {e.tow_reference} vehicle_id = {e.vehicle_id} comments = {e.comments} keys = {e.k} front_pic = {e.front_pic} back_pic = {e.back_pic} />
-                </div>
-              )
-            if (results.length === 0) message.error('No Results Found');
-            return (results)
-            // search by make and model
-          } else if (year === "" && make !== "" && model !== "" && key === "" && reason === "") {
-             let results =
-             this.state.loading === true ? <Spin /> :
-             this.state.data
-               .filter(x => x.make === this.props.searchMake && x.model === this.props.searchModel)
-               .map((e, key) =>
-                 <div key = {key}>
-                   <SearchItem year = {e.year} make = {e.make} model = {e.model} vin = {e.vin} lot = {e.lot} reason = {e.reason} tow_reference = {e.tow_reference} vehicle_id = {e.vehicle_id} comments = {e.comments} keys = {e.k} front_pic = {e.front_pic} back_pic = {e.back_pic} />
-                 </div>
-               )
-             if (results.length === 0) message.error('No Results Found');
-             return (results)
-          }
+    } else if (year === "" && make === "" && model !== "" && key === "" && reason === "") {
+        return (
+          this.state.loading === true ? <Spin /> : this.state.data.filter(x => x.model === this.props.searchModel).map((e, key) =>
+            <div key = {key}>
+              <SearchItem year = {e.year} make = {e.make} model = {e.model} vin = {e.vin} lot = {e.lot} reason = {e.reason} tow_reference = {e.tow_reference} vehicle_id = {e.vehicle_id} comments = {e.comments} keys = {e.k} front_pic = {e.front_pic} back_pic = {e.back_pic} />
+            </div>
+          ).slice(this.state.pageStart, this.state.pageEnd)
+        )
+      // search by only keys
+    } else if (year === "" && make === "" && model === "" && key !== "" && reason === "") {
+        return (
+          this.state.loading === true ? <Spin /> : this.state.data.filter(x => (x.k === 'K' ? 'Yes' : 'No') === this.props.searchKey).map((e, key) =>
+          <div key = {key}>
+            <SearchItem year = {e.year} make = {e.make} model = {e.model} vin = {e.vin} lot = {e.lot} reason = {e.reason} tow_reference = {e.tow_reference} vehicle_id = {e.vehicle_id} comments = {e.comments} keys = {e.k} front_pic = {e.front_pic} back_pic = {e.back_pic} />
+          </div>
+          ).slice(this.state.pageStart, this.state.pageEnd)
+        )
+      // search by only reason
+    } else if (year === "" && make === "" && model === "" && key === "" && reason !== "") {
+        return (
+          this.state.loading === true ? <Spin /> : this.state.data.filter(x => x.reason === this.props.searchReason).map((e, key) =>
+            <div key = {key}>
+              <SearchItem year = {e.year} make = {e.make} model = {e.model} vin = {e.vin} lot = {e.lot} reason = {e.reason} tow_reference = {e.tow_reference} vehicle_id = {e.vehicle_id} comments = {e.comments} keys = {e.k} front_pic = {e.front_pic} back_pic = {e.back_pic} />
+            </div>
+          ).slice(this.state.pageStart, this.state.pageEnd)
+        )
+    // search by year and make
+    } else if (year !== "" && make !== "" && model === "" && key === "" && reason === "") {
+      let results =
+      this.state.loading === true ? <Spin /> :
+      this.state.data
+        .filter(x => x.year === this.props.searchYear && x.make === this.props.searchMake)
+        .map((e, key) =>
+          <div key = {key}>
+            <SearchItem year = {e.year} make = {e.make} model = {e.model} vin = {e.vin} lot = {e.lot} reason = {e.reason} tow_reference = {e.tow_reference} vehicle_id = {e.vehicle_id} comments = {e.comments} keys = {e.k} front_pic = {e.front_pic} back_pic = {e.back_pic} />
+          </div>
+        ).slice(this.state.pageStart, this.state.pageEnd)
+      if (results.length === 0) message.error('No Results Found');
+      return (results)
+     // search by year and model
+     } else if (year !== "" && make === "" && model !== "" && key === "" && reason === "") {
+        let results =
+        this.state.loading === true ? <Spin /> :
+        this.state.data
+          .filter(x => x.year === this.props.searchYear && x.model === this.props.searchModel)
+          .map((e, key) =>
+            <div key = {key}>
+              <SearchItem year = {e.year} make = {e.make} model = {e.model} vin = {e.vin} lot = {e.lot} reason = {e.reason} tow_reference = {e.tow_reference} vehicle_id = {e.vehicle_id} comments = {e.comments} keys = {e.k} front_pic = {e.front_pic} back_pic = {e.back_pic} />
+            </div>
+          ).slice(this.state.pageStart, this.state.pageEnd)
+        if (results.length === 0) message.error('No Results Found');
+        return (results)
+        // search by make and model
+      } else if (year === "" && make !== "" && model !== "" && key === "" && reason === "") {
+         let results =
+         this.state.loading === true ? <Spin /> :
+         this.state.data
+           .filter(x => x.make === this.props.searchMake && x.model === this.props.searchModel)
+           .map((e, key) =>
+             <div key = {key}>
+               <SearchItem year = {e.year} make = {e.make} model = {e.model} vin = {e.vin} lot = {e.lot} reason = {e.reason} tow_reference = {e.tow_reference} vehicle_id = {e.vehicle_id} comments = {e.comments} keys = {e.k} front_pic = {e.front_pic} back_pic = {e.back_pic} />
+             </div>
+           ).slice(this.state.pageStart, this.state.pageEnd)
+         if (results.length === 0) message.error('No Results Found');
+         return (results)
+      }
     }
 
     render() {
       return (
         <div style = {{textAlign: 'center', marginTop: 20}}>
           {this.returnResults(this.props.searchYear, this.props.searchMake, this.props.searchModel, this.props.searchKey, this.props.searchReason)}
+          <Pagination
+            showQuickJumper
+            style = {{margin: 20}}
+            defaultCurrent={1}
+            pageSize = {9}
+            onChange={this.onChange}
+            total={this.state.data.length} />
         </div>
       )
     }
